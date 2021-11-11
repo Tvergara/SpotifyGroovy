4324f2dba2bcde6191c2b398d29660bd36a9626e
Tvergara <tomvergara@uc.cl>
2021-11-11 11:04:28 -0300

    fix(): fix stop regex

--- handlers/stop_handler.py
+++ handlers/stop_handler.py
@@ -2,7 +2,7 @@ from handlers.base import BaseHandler
 
 class StopHandler(BaseHandler):
     def pattern(self):
-        return r'-stop(\S*)$'
+        return r'-stop(\s*)$'
       
     def handle(self):
         self.cast.media_controller.stop()
