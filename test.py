4a077e1fdd0ae545cf6a24f2938e77f8c4809877
Tvergara <tomvergara@uc.cl>
2021-11-25 14:59:15 -0300

    chore(): delete test file

--- test.py
+++
@@ -1,6 +0,0 @@
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-from spotipy.oauth2 import SpotifyOAuth
-
-spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(scope='streaming user-modify-playback-state', redirect_uri='https://google.com'))
-spotify.pause_playback()
