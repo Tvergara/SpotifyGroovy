c77989034a17eebacf57ffcbd161bb88f19d5924
Tvergara <tomvergara@uc.cl>
2021-11-25 11:02:27 -0300

    feat(): set client token

--- main.py
+++ main.py
@@ -3,15 +3,47 @@ import os
 from threading import Thread
 import json
 from dotenv import load_dotenv
+from werkzeug.utils import redirect
 from handlers import *
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from spotipy.oauth2 import SpotifyOAuth
+import requests
+import time
 
 load_dotenv()
 
 app = Flask(__name__)
 VERIFICATION_TOKEN = os.getenv('VERIFICATION_TOKEN')
+CLIENT_ID = os.getenv('SPOTIPY_CLIENT_ID')
+APP_URL = os.getenv('APP_URL')
+CLIENT_SECRET = os.getenv('SPOTIPY_CLIENT_SECRET')
+
+@app.route('/', methods=['GET'])
+def set_token():
+    print('Token received')
+    code = request.query_string.decode().split('=')[1]
+    body = {}
+    body['grant_type'] = 'authorization_code'
+    body['code'] = code
+    body['redirect_uri'] = APP_URL 
+    body['client_id'] = CLIENT_ID
+    body['client_secret'] = CLIENT_SECRET
+
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    response = requests.post('https://accounts.spotify.com/api/token', data=body, headers=headers)
+    token = json.loads(response.text)
+    token['expires_at'] = int(time.time()) + token['expires_in']
+    
+    with open('.cache', 'w') as f:
+        f.write(str(token).replace("'", '"'))
+  
+    return 'success'
+
+@app.route('/account', methods=['GET'])
+def set_account():
+    url = APP_URL.replace('/', '%2F')
+    return redirect(f'https://accounts.spotify.com/es-ES/authorize?client_id={CLIENT_ID}&response_type=code&redirect_uri={url}&scope=streaming%20user-modify-playback-state')
 
 @app.route("/", methods=['POST'])
 def event_hook():
